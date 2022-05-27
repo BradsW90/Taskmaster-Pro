aa294ec70960775baea6669e03d27ac6f0dcea96
Amanda Hefner <bswj90@gmail.com>
2022-05-26 23:18:50 -0400

    stopping for the night

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,9 +78,7 @@ $(".list-group").on("click", "span", function () {
 
 $(".list-group").on("blur", "input[type='text']", function () {
   var date = $(this).val().trim();
-
   var status = $(this).closest(".list-group").attr("id").replace("list-", "");
-
   var index = $(this).closest(".list-group-item").index();
 
   tasks[status][index].date = date;
